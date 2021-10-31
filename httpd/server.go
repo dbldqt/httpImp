87367ef514ae2be7f409bdb57dd42243feeb27c1
unknown <wu869022133@126.com>
2021-10-31 22:59:23 +0800

    static route

--- httpd/server.go
+++ httpd/server.go
@@ -4,10 +4,6 @@ import (
 	"net"
 )
 
-type Handler interface {
-	ServeHTTP(w ResponseWriter,r *Request)
-}
-
 type Server struct {
 	Addr string
 	Handler Handler
@@ -28,4 +24,71 @@ func (s *Server)ListenAndServe() error{
 		conn := newConn(rawConn,s)
 		go conn.Serve()
 	}
+}
+
+type HandlerFunc func(w ResponseWriter,r *Request)
+
+type Handler interface {
+	ServeHTTP(w ResponseWriter,r *Request)
+}
+
+type ServeMux struct {
+	m map[string]HandlerFunc
+}
+
+func NewServerMux() *ServeMux {
+	return &ServeMux{
+		m:make(map[string]HandlerFunc),
+	}
+}
+
+func (sm *ServeMux)HandleFunc(pattern string,cb HandlerFunc) {
+	if sm.m == nil {
+		sm.m = make(map[string]HandlerFunc)
+	}
+	sm.m[pattern] = cb
+}
+
+func (sm *ServeMux) Handle(pattern string,handler Handler) {
+	if sm.m == nil {
+		sm.m = make(map[string]HandlerFunc)
+	}
+	sm.m[pattern] = handler.ServeHTTP
+}
+
+func (sm *ServeMux) ServeHTTP(w ResponseWriter, r *Request) {
+	handler, ok := sm.m[r.Url.Path]
+	if !ok {
+		if len(r.Url.Path) > 1 && r.Url.Path[len(r.Url.Path)-1] == '/' {
+			handler, ok = sm.m[r.Url.Path[:len(r.Url.Path)-1]]
+		}
+		if !ok {
+			w.WriteHeader(StatusNotFound)
+			return
+		}
+	}
+	handler(w, r)
+}
+
+var defaultServeMux ServeMux
+
+var DefaultServeMux = &defaultServeMux
+
+func HandleFunc(pattern string, cb HandlerFunc) {
+	DefaultServeMux.HandleFunc(pattern, cb)
+}
+
+func Handle(pattern string, handler Handler) {
+	DefaultServeMux.Handle(pattern, handler)
+}
+
+func ListenAndServe(addr string, handler Handler) error {
+	if handler == nil {
+		handler = DefaultServeMux
+	}
+	svr := &Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+	return svr.ListenAndServe()
 }
